2f4a30fc0332d41218873110effaa2ca2acad4c8
sb-gerald-g <82987641+sb-gerald-g@users.noreply.github.com>
2021-07-31 13:13:55 -0400

    Added delete button
    Added feature where a delete button is added to every row, except for the first row, which deletes the corresponding row. Row numbers are also updated in a separate function. The code that takes in user input has also been updated and put in a separate function to accurately update the user input if a row is deleted

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-var invoice = new Array(7);
+var invoice;
 var invoiceValues;
 
 document.onkeyup = function(){setUp()};
 
 function setUp() {
-	invoice = document.getElementsByName("row");
-	invoiceValues = new Array(invoice.length);
-	var i = 0;
-
-	for (i = 0; i < invoice.length; i++){
-		invoiceValues[i] = invoice[i].value;
-	}
+	recordForm();
 
 	if (Validate()){
 		AddRow();
+	}
+}
+
+function recordForm(){
+	invoice = new Array();
+	invoice = document.getElementsByName("row");
+	invoiceValues = new Array(invoice.length);
+	
+	var i = 0;
+	for (i = 0; i < invoice.length; i++){
+		invoiceValues[i] = invoice[i].value;
 	}
 }
 
@@ -21,7 +26,6 @@ function Validate() {
 	var row = 0;
 	var rowMax = invoice.length / 7;
 	var column = 0;
-	var complete = true;
 
 	for (row = 0; row < rowMax; row++){
 		for (column = 0; column < 7; column++){
@@ -37,11 +41,28 @@ function AddRow() {
 	var table = document.getElementById("table");
 	var rowNumber = table.rows.length;
 	var row = table.insertRow(rowNumber);
-	row.insertCell(0).innerHTML = rowNumber;
+	row.insertCell(0).innerHTML = '<p name="rowNumber">' + rowNumber;
 	row.insertCell(1).innerHTML = '<input type="text" name ="row" value ="" placeholder="empty">';		// Service
 	row.insertCell(2).innerHTML = '<input type="text" name ="row" value ="" placeholder="empty">';		// Item
 	row.insertCell(3).innerHTML = '<input type="text" name ="row" value ="" placeholder="optional">';	// Description
 	row.insertCell(4).innerHTML = "$" + '<input type="number" name ="row" value ="" placeholder="0.00">' + " " + '<input type="text" name ="row" value ="" placeholder="Unit">';	// Rate
 	row.insertCell(5).innerHTML = '<input type="number" name ="row" value ="" placeholder="0">';		//Qty
-	row.insertCell(6).innerHTML = '<input type="number" name ="row" value ="" placeholder="0.00">';		// Price
+	row.insertCell(6).innerHTML = "$" +'<input type="number" name ="row" value ="" placeholder="0.00">';		// Price
+
+	var button = row.insertCell(7);
+	button.innerHTML = '<input type="button" value="delete" onclick=deleteRow(this)>'
+}
+
+function deleteRow(row){
+	row.parentNode.parentNode.remove();
+	recordForm();
+	reNumber();
+}
+
+function reNumber(){
+	var rowNumbers = document.getElementsByName("rowNumber");
+	var i;
+	for (i = 0; i < rowNumbers.length; i++){
+		rowNumbers[i].innerHTML = (i + 2);
+	}
 }
